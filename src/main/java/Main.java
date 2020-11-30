e0f3a97b758c64db6f7135d66c9ffdca1ddf0caf
Hyeon9mak <jinha3507@gmail.com>
2020-11-30 17:44:59 +0900

    del Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.function.ToDoubleBiFunction;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+class Main {
+
+    public static class BasicCompareFruits implements Comparable<BasicCompareFruits> {
+        private Integer price;
+
+        @Override
+        public int compareTo(BasicCompareFruits basicCompareFruits) {
+            return Integer.compare(price, basicCompareFruits.price);
+        }
+    }
+
+    public static class RelationalCompareFruits implements Comparable<RelationalCompareFruits> {
+        private Integer price;
+
+        @Override
+        public int compareTo(RelationalCompareFruits relationalCompareFruits) {
+            if (price == relationalCompareFruits.price) {
+                return 0;
+            }
+            else if (price > relationalCompareFruits.price) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    public static void main(String[] argc){
+        String s1 = new String("wow");
+        String s2 = new String("wow");
+
+        Object.hashCode(s1);
+
+    }
 }
